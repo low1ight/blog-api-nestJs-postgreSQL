07b2918a033e5455fa7ecd499d87b8896a9f9de2
Maksym <qlowlight@gmail.com>
2023-06-16 12:23:37 +0300

    add pg, add typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersController } from './users/users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserQueryRepository } from './users/repository/user.query.repository';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.local', '.env'],
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'nodeJs',
+      password: 'nodeJs',
+      database: 'Blog',
+      autoLoadEntities: false,
+      synchronize: false,
+    }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UserQueryRepository],
 })
 export class AppModule {}
