6ec006e4ebc7980d74574909c105f427f08dfc79
Maksym <qlowlight@gmail.com>
2023-06-22 18:27:20 +0300

    add local strategy, add local.auth.guard

---
+++ src/modules/users_module/auth/strategies/local.strategy.ts
@@ -0,0 +1,27 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthPublicService } from '../application/public/auth-public.service';
+import { UserForLoginValidationModel } from '../../users/repositories/dto/UserForLoginValidationModel';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthPublicService) {
+    super({ usernameField: 'loginOrEmail' });
+  }
+
+  async validate(loginOrEmail: string, password: string): Promise<any> {
+    const user: UserForLoginValidationModel | null =
+      await this.authService.validateUser(loginOrEmail, password);
+    if (!user) {
+      throw new UnauthorizedException('wrong email or login');
+    }
+    if (!user.isConfirmed) {
+      throw new UnauthorizedException('email is not confirmed');
+    }
+    if (user.isBanned) {
+      throw new UnauthorizedException('user has been banned');
+    }
+    return user;
+  }
+}
