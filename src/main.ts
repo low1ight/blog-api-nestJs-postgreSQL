1c1899de2a502adc174494e7a28ebac95e011c0a
Maksym <qlowlight@gmail.com>
2023-06-22 19:22:46 +0300

    add cookie-parser, refresh token return now in cookie

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
 
   app.useGlobalPipes(
     new ValidationPipe({
--- src/modules/users_module/auth/controllers/auth.public.controller.ts
+++ src/modules/users_module/auth/controllers/auth.public.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Ip, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from '../guards/local.auth.guard';
 import { CommandBus } from '@nestjs/cqrs';
+import { Response } from 'express';
 import { LoginUseCaseCommand } from '../application/public/useCase/login-use-case';
 
 @Controller('auth')
@@ -15,8 +16,15 @@ export class AuthPublicController {
   ) {
     const { id, login } = req.user;
     const title = req.headers['user-agent'];
-    return await this.commandBus.execute(
+    const { refreshToken, accessToken } = await this.commandBus.execute(
       new LoginUseCaseCommand(id, login, title, ip),
     );
+
+    response.cookie('refreshToken ', refreshToken, {
+      // httpOnly: true,
+      // secure: true,
+    });
+
+    return { accessToken };
   }
 }
