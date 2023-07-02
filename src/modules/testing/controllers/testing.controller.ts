e73613883672a774de59816d6878c5f954cc6808
Maksym <qlowlight@gmail.com>
2023-07-02 14:29:16 +0300

    set 204 success code for delete-all-data

--- src/modules/testing/controllers/testing.controller.ts
+++ src/modules/testing/controllers/testing.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Delete } from '@nestjs/common';
+import { Controller, Delete, HttpCode } from '@nestjs/common';
 import { TestingService } from '../testing.service';
 
 @Controller('testing')
 export class TestingController {
   constructor(private testingService: TestingService) {}
   @Delete('all-data')
+  @HttpCode(204)
   async deleteAllDataFromTables() {
     await this.testingService.deleteAllData();
   }
