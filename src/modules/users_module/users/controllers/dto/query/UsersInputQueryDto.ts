a84d33da5d02aead7d9a763dbf137bb5b74d235e
Maksym <qlowlight@gmail.com>
2023-07-19 17:35:19 +0300

    add users query input dto

---
+++ src/modules/users_module/users/controllers/dto/query/UsersInputQueryDto.ts
@@ -0,0 +1,9 @@
+export type UserInputQueryDto = {
+  searchLoginTerm: string | undefined;
+  searchEmailTerm: string | undefined;
+  sortBy: string | undefined;
+  sortDirection: string | undefined;
+  pageNumber: string | undefined;
+  pageSize: string | undefined;
+  banStatus: string | undefined;
+};
