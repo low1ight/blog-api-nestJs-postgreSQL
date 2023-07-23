ce88e0e156c82b4bb5e20df10495a9a606534288
Maksym <qlowlight@gmail.com>
2023-07-23 15:01:16 +0300

    add get blog posts endpoint for public

--- src/modules/blog_platform/blogs/controllers/blogs.public.controller.ts
+++ src/modules/blog_platform/blogs/controllers/blogs.public.controller.ts
@@ -1,14 +1,31 @@
 import { BlogsQueryRepository } from '../repositories/query-repository/blogs-query-repository';
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { BlogQueryInputDto } from './dto/query/BlogQueryInputDto';
 import { BlogPaginator } from './dto/query/BlogPaginator';
+import { PostsPaginator } from '../../posts/controllers/dto/query/PostsPaginator';
+import { PostsQueryDto } from '../../posts/controllers/dto/query/PostsQueryDto';
+import { CustomParseInt } from '../../../../common/customPipe/customParseInt';
+import { PostsQueryRepository } from '../../posts/repository/posts-query-repository.service';
 @Controller('blogs')
 export class BlogsPublicController {
-  constructor(private readonly blogsQueryRepository: BlogsQueryRepository) {}
+  constructor(
+    private readonly blogsQueryRepository: BlogsQueryRepository,
+    private readonly postsQueryRepository: PostsQueryRepository,
+  ) {}
   @Get('')
   async getBlogs(@Query() query: BlogQueryInputDto) {
     const paginator = new BlogPaginator(query);
 
     return await this.blogsQueryRepository.getAllBlogs(null, paginator);
+  }
+
+  @Get(':id/posts')
+  async getPostsForBlogs(
+    @Query() query: PostsQueryDto,
+    @Param('id', CustomParseInt) id: number,
+  ) {
+    const paginator = new PostsPaginator(query);
+
+    return await this.postsQueryRepository.getBlogPosts(id, paginator);
   }
 }
