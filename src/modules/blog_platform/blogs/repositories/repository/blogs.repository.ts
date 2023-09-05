3271f7c2ddbe3056ea03b2ed1bed809d05ad8e09
low1ight <qlowlight@gmail.com>
2023-09-05 15:35:11 +0300

    refactoring blog repo using typeORM repository

--- src/modules/blog_platform/blogs/repositories/repository/blogs.repository.ts
+++ src/modules/blog_platform/blogs/repositories/repository/blogs.repository.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBlogDto } from '../../controllers/dto/CreateBlogDto';
-import { InjectDataSource } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { BlogViewModel } from '../dto/BlogViewModel';
 import { UpdateBlogDto } from '../../controllers/dto/UpdateBlogDto';
+import { Blog } from '../../entity/Blog.entity';
 
 @Injectable()
 export class BlogsRepository {
-  constructor(@InjectDataSource() protected dataSource: DataSource) {}
+  constructor(
+    @InjectDataSource() protected dataSource: DataSource,
+    @InjectRepository(Blog) protected blogsRepository: Repository<Blog>,
+  ) {}
 
   async createBlog({
     ownerId,
@@ -16,117 +20,67 @@ export class BlogsRepository {
     websiteUrl,
     isMembership,
   }: CreateBlogDto): Promise<BlogViewModel> {
-    const result = await this.dataSource.query(
-      `
-    
-        INSERT INTO public."Blogs"(
-         "ownerId", "name", "description", "websiteUrl", "isMembership", "createdAt")
-        VALUES ( $1, $2, $3, $4, $5, now())
-        RETURNING "id", "name", "description", "websiteUrl", "isMembership", "createdAt"
-    
-        `,
-      [ownerId, name, description, websiteUrl, isMembership],
-    );
+    const blog = new Blog();
 
-    return new BlogViewModel(result[0]);
+    blog.ownerId = ownerId;
+    blog.name = name;
+    blog.description = description;
+    blog.websiteUrl = websiteUrl;
+    blog.isMembership = isMembership;
+    blog.createdAt = new Date();
+
+    const createdBlog = await this.blogsRepository.save(blog);
+
+    return new BlogViewModel(createdBlog);
   }
 
-  async getBlogById(blogId) {
-    const blog = await this.dataSource.query(
-      `
-    
-    SELECT * FROM "Blogs"
-    WHERE "id" = $1
-    
-    `,
-      [blogId],
-    );
+  async getBlogById(blogId: number) {
+    const blog = await this.blogsRepository.findOneBy({ id: blogId });
 
-    return blog[0] || null;
+    return blog || null;
   }
 
   async updateBlog(
     blogId: number,
     { name, description, websiteUrl }: UpdateBlogDto,
   ) {
-    return await this.dataSource.query(
-      `
+    const blog = await this.blogsRepository.findOneBy({ id: blogId });
 
-    UPDATE public."Blogs"
-    SET name=$2, description=$3, "websiteUrl"=$4
-    WHERE "id" = $1;
-    
-    
-    `,
-      [blogId, name, description, websiteUrl],
-    );
+    blog.name = name;
+    blog.description = description;
+    blog.websiteUrl = websiteUrl;
+
+    await this.blogsRepository.save(blog);
   }
 
   async deleteBlogById(blogId: number) {
-    return await this.dataSource.query(
-      `
-    
-    DELETE FROM public."Blogs"
-    WHERE id=$1;
-    
-    
-    `,
-      [blogId],
-    );
+    await this.blogsRepository.delete({ id: blogId });
   }
 
   async getBlogOwnerId(blogId: number): Promise<number | null> {
-    const result = await this.dataSource.query(
-      `
-    
-    SELECT "ownerId" FROM "Blogs" WHERE "id" = $1
-    
-    
-    
-    `,
-      [blogId],
-    );
+    const blog = await this.blogsRepository.findOneBy({ id: blogId });
 
-    return result[0]?.ownerId || null;
+    return blog?.ownerId || null;
   }
 
   async bindUserForBlog(blogId: number, userId: number) {
-    await this.dataSource.query(
-      `
-    
-   UPDATE public."Blogs"
-   SET "ownerId"=$2
-   WHERE "id" = $1;
-    
-    `,
-      [blogId, userId],
-    );
+    const blog = await this.blogsRepository.findOneBy({ id: blogId });
+
+    blog.ownerId = userId;
+    await this.blogsRepository.save(blog);
   }
 
   async getBlogBanStatusById(blogId: number) {
-    const blog = await this.dataSource.query(
-      `
-    
-    SELECT "isBanned" FROM "Blogs"
-    WHERE "id" = $1
-    
-    `,
-      [blogId],
-    );
+    const blog = await this.blogsRepository.findOneBy({ id: blogId });
 
-    return blog[0] || null;
+    return blog || null;
   }
 
   async setBanStatusForBlog(banStatus: boolean, blogId: number) {
-    await this.dataSource.query(
-      `
-    
-    UPDATE public."Blogs"
-    SET "isBanned"=$1, "banDate"=${banStatus ? 'now()' : null}
-    WHERE "id" = $2;
-    
-    `,
-      [banStatus, blogId],
-    );
+    const blog = await this.blogsRepository.findOneBy({ id: blogId });
+    blog.isBanned = banStatus;
+    blog.banDate = banStatus ? new Date() : null;
+
+    await this.blogsRepository.save(blog);
   }
 }
