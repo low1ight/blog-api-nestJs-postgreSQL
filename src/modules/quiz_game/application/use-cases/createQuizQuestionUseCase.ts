61e443d54ff3739d0e41db4a108c835b07d33027
low1ight <qlowlight@gmail.com>
2023-12-08 16:49:08 +0200

    add create new quiz use case

---
+++ src/modules/quiz_game/application/use-cases/createQuizQuestionUseCase.ts
@@ -0,0 +1,18 @@
+import { CreateQuizQuestionInputDto } from '../../controllets/dto/CreateQuizQuestionInputDto';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { QuizQuestionsSaRepo } from '../../repository/quiz.questions.sa.repo';
+
+export class CreateQuizQuestionUseCaseCommand {
+  constructor(public dto: CreateQuizQuestionInputDto) {}
+}
+
+@CommandHandler(CreateQuizQuestionUseCaseCommand)
+export class CreateQuizQuestionUseCase
+  implements ICommandHandler<CreateQuizQuestionUseCaseCommand>
+{
+  constructor(private readonly quizQuestionSaRepo: QuizQuestionsSaRepo) {}
+
+  async execute({ dto }: CreateQuizQuestionUseCaseCommand) {
+    return await this.quizQuestionSaRepo.createQuizQuestion(dto);
+  }
+}
